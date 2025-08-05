2bd8b3d4902c0c3c7e88c1cac302f62c3cf8fe4e
marketinghelpdesk1 <133745805+marketinghelpdesk1@users.noreply.github.com>
2025-08-05 09:43:48 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 
 print("SLACK_BOT_TOKEN:", os.environ.get("SLACK_BOT_TOKEN"))
+print("SERVICENOW_USER:", os.environ.get("SERVICENOW_USER"))
 
 SERVICENOW_INSTANCE = "dev351449.service-now.com"
 SERVICENOW_USER = "admin"
