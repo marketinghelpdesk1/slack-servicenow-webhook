c23ae6965b80fd0aa05d1fb6c70d96ac08d30b71
marketinghelpdesk1 <133745805+marketinghelpdesk1@users.noreply.github.com>
2025-08-05 08:03:25 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.INFO)
 SERVICENOW_INSTANCE = "dev351449.service-now.com"
 SERVICENOW_USER = "admin"
 SERVICENOW_PASSWORD = "az5CI1uA!Mm@"
-SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN", "xoxb-9290586945379-9317287104928-1dFw1AvbaJKXIoIJ0EtVSkxA")
+SLACK_BOT_TOKEN = os.environ.get("SLACK_BOT_TOKEN", "xoxb-9290586945379-9317287104928-SVCzbqMbAf8dfdvoxQVUf703")
 
 @app.route('/')
 def index():
